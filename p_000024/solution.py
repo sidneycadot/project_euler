932e7530ab2d11a5d914aa5c41bd7ba40a5c8ce5
Sidney Cadot <sidney@jigsaw.nl>
2024-09-01 09:29:51 +0200

    Solutions up to and including problem 25.

---
+++ p_000024/solution.py
@@ -0,0 +1,60 @@
+#! /usr/bin/env python3
+
+"""
+A permutation is an ordered arrangement of objects. For example, 3124 is one possible permutation of the digits 1, 2, 3 and 4.
+If all of the permutations are listed numerically or alphabetically, we call it lexicographic order.
+The lexicographic permutations of 0, 1 and 2 are:
+
+    012   021   102   120   201   210
+
+What is the millionth lexicographic permutation of the digits 0, 1, 2, 3, 4, 5, 6, 7, 8 and 9?
+"""
+
+import math
+
+def generate_lexicographical_order(elements):
+    """Generate all permutations of distinct objects.
+
+    If the elements are sorted, the elements will be yielded in lexicographical order.
+
+    This is equivalent to the itertools.permutations() iterator.
+    """
+    if len(elements) == 0:
+        yield ()
+    else:
+        for (i, head) in enumerate(elements):
+            tail_elements = elements[:i] + elements[i + 1:]
+            yield from ((head, ) + tail for tail in generate_lexicographical_order(tail_elements))
+
+def nth_permutation(elements, n: int):
+    """Return the nth permutation of the given elements."""
+
+    sz = len(elements)
+
+    if sz == 0:
+        return ()
+
+    period = math.factorial(sz - 1)
+
+    head_index = (n // period) % sz
+    tail_index = n % period
+
+    head = elements[head_index]
+
+    tail_elements = elements[:head_index] + elements[head_index + 1:]
+
+    tail = nth_permutation(tail_elements, tail_index)
+
+    return (head, ) + tail
+
+
+def main():
+
+    # Note: the one millionth element has index 999999.
+    arrangement = nth_permutation(list(range(10)), 999999)
+    solution = "".join(str(e) for e in arrangement)
+    print("solution:", solution)
+
+
+if __name__ == "__main__":
+    main()
---
+++ p_000025/solution.py
@@ -0,0 +1,41 @@
+#! /usr/bin/env python3
+
+"""
+The Fibonacci sequence is defined by the recurrence relation:
+
+    F(n) = F(n - 1) + F(n - 2), where F(1) = 1 and F(2) = 1.
+
+Hence the first 12 terms will be:
+
+    F(1) = 1
+    F(2) = 1
+    F(3) = 2
+    F(4) = 3
+    F(5) = 5
+    F(6) = 8
+    F(7) = 13
+    F(8) = 21
+    F(9) = 34
+    F(10) = 55
+    F(11) = 89
+    F(12) = 144
+
+The 12th term, F(12) is the first term to contain three digits.
+
+What is the index of the first term in the Fibonacci sequence to contain 1000 digits?"""
+
+
+def main():
+
+    idx = 2
+    (f1, f2) = 1, 1
+
+    while len(str(f2)) != 1000:
+        (f1, f2) = (f2, f1 + f2)
+        idx += 1
+
+    print("solution:", idx)
+
+
+if __name__ == "__main__":
+    main()
